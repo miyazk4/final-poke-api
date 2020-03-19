d94d42a0c5c3171408a0133f2ddb50d7a6227acf
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-19 00:30:09 +0000

    Added content to display on page

--- src/pages/pokemon/index.tsx
+++ src/pages/pokemon/index.tsx
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Pokemon from "./pokemon";
 
-const Pokemon = () => {
-  return <div>gonna catch them all ...</div>;
+export interface PokeData {
+  name: string;
+  url?: string;
+  base_experience?: number;
+  types: Types[];
+  abilities: Abilities[];
+  weight: number;
+  height: number;
+  sprites: {
+    front_default: string;
+  };
+}
+
+interface Types {
+  slot?: number;
+  type: {
+    name: string;
+  };
+}
+
+interface Abilities {
+  ability: {
+    name: string;
+    url?: string;
+  };
+}
+
+// url pq ele há de ter o pokemon.url, tal como tem o pokemon.name
+const GET = (url: string) => fetch(url).then(r => r.json());
+/*
+async function fetchAllPokemons(url: string): Promise<PokeResult[]> {
+    const list = await GET(url);
+    return await Promise.all(list.map(pk => GET(pk.url)));
+}
+fetchAllPokemons('https://pokeurl ')
+
+*/
+
+const PokePage = () => {
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+  const [pokemonData, setPokemonData] = useState<PokeData[]>([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getPokeData();
+  }, []);
+
+  const getPokeData = async () => {
+    let response = await fetch(initialUrl);
+    const data = await response.json();
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+    console.log(data.results);
+
+    const pokeRecord: PokeData[] = await Promise.all(
+      data.results.map((pk: any) => GET(pk.url))
+    );
+
+    console.log(pokeRecord);
+
+    setPokemonData(pokeRecord);
+  };
+
+  return (
+    <div>
+      <h1>In the future i'm going to catch them all...</h1>
+      <div className="pokeData">
+        {pokemonData.map(pokemon => (
+          <Pokemon pokemon={pokemon} key={pokemon.name} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Pokemon;
+// Pokemon = {pokemon} pq contém tudo, escusa de estar name = {pokemon name} etc
+
+export default PokePage;
--- src/pages/pokemon/pokemon.tsx
+++ src/pages/pokemon/pokemon.tsx
@@ -6,7 +6,26 @@ interface PokeProps {
 }
 
 const Pokemon = ({ pokemon }: PokeProps) => {
-  return <div className="pokeContainer"></div>;
+  return (
+    <div className="pokeContainer">
+      <div>
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      </div>
+      <h1>{pokemon.name}</h1>
+      <div>
+        {pokemon.types.map((type: any) => (
+          <li>{type.type.name}</li>
+        ))}
+      </div>
+      <div>{pokemon.weight}</div>
+      <div>{pokemon.height}</div>
+      <div>
+        {pokemon.abilities.map((ability: any) => (
+          <p>{ability.ability.name}</p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Pokemon;
