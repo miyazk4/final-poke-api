bb9cd4eb8d86a088c6afaa6197423c1c69f2d07b
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-24 01:06:32 +0000

    Gave content and style to e404

--- src/pages/e404.tsx
+++ src/pages/e404.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./e404.scss";
+import { Link } from "react-router-dom";
 
 const E404 = () => {
-  let location = useLocation();
+  //let location = useLocation();
 
   return (
-    <div>
-      <h1>
-        Error 404 
-      </h1>
-      <p>
-        The path <code>{location.pathname}</code> does not exist
-      </p>
+    <div className="errorContainer">
+      <h1 className="errorTitle">Error 404</h1>
+      <div className="errorDescription">
+        <p>The page you were looking for does not exist.</p>
+        <p>
+          Go back to <Link to="/">homepage</Link>
+        </p>
+      </div>
     </div>
   );
 };
