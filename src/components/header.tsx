3e7bb2c532ac6f1df6f4193db4bc2b8a486feaca
Joana Santos <joanasantos@Joanas-MacBook-Pro.local>
2020-03-24 00:07:13 +0000

    Styled header on max width 600

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,22 @@
-import React, { useState } from "react";
-import cn from "classnames";
+import React from "react";
 import pokeLogo from "../images/pokeapi_256.png";
 import { Link } from "react-router-dom";
 import styles from "./header.module.scss";
-import Navigation from "./navigation";
+
 import Hamburger from "./hamburgerMenu";
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-
-  const toggle = () => {
-    setOpen(!open);
-  };
-
-  /*const toggle = (x: any) => {
-    x.classList.toggle("change");
-  }; */
-
   return (
     <header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          <h1>
+          <h1 className={styles.logo}>
             <Link to="/">
-              <img src={pokeLogo} alt="Poke Api Logo" />
+              <img
+                className={styles.logoImage}
+                src={pokeLogo}
+                alt="Poke Api Logo"
+              />
             </Link>
           </h1>
         </div>
